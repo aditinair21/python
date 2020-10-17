5d799944144ee71081817e8eff39d0c8b809dad0
aditinair21 <60553955+aditinair21@users.noreply.github.com>
2020-10-17 13:15:27 -0700

    Add files via upload

---
+++ MadLib.py
@@ -0,0 +1,8 @@
+#MadLib.py
+adjective= input("Please enter an adjective: ")
+noun= input("Please enter a noun: ")
+verb= input("Please enter a verb ending in -ed: ")
+adverb= input("Please enter an adverb: ")
+name= input("Enter a name: ")
+print("Your MadLib:")
+print("The", adjective, noun,",",name,",", adverb, verb, "over the lazy dog.") 
---
+++ rectangle.py
@@ -0,0 +1,10 @@
+import turtle
+t= turtle.Pen()
+t.speed(-40)
+t.forward(100)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(50)
